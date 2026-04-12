068a540cddb8920c1f048183746a0f163be63e0d
Leo Daidone <leo.daidone@gmail.com>
2026-04-12 17:35:49 -0300

    Monster's first breath. - Add public/index - Created src tree skeleton - Implemented SSE heartbeating - Update composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'frankenforge/kernel',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '35da1faf39e0deaf6a097c3f24101c4cff97d015',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'frankenforge/kernel' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '35da1faf39e0deaf6a097c3f24101c4cff97d015',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
